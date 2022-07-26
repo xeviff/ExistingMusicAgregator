3347615c8ab67f3fbf79e58c63b1ac37b2f2b0f4
xeviff <xavier@hack3.cat>
2022-07-26 17:22:12 +0200

    main program skeleton

---
+++ src/main/java/cat/hack3/mangrana/music/drive/handler/TemporaryFolderAgregator.java
@@ -0,0 +1,13 @@
+package cat.hack3.mangrana.music.drive.handler;
+
+public class TemporaryFolderAgregator {
+
+    public static void main(String[] args) {
+        new TemporaryFolderAgregator().moveOneArtist();
+    }
+
+    private void moveOneArtist() {
+
+    }
+
+}
