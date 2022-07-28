245c977c14998492b9a74550802fd73387efcbb1
xeviff <xavier@hack3.cat>
2022-07-28 16:53:14 +0200

    artist copy service

---
+++ src/main/java/cat/hack3/mangrana/google/api/client/ArtistCopyService.java
@@ -0,0 +1,27 @@
+package cat.hack3.mangrana.google.api.client;
+
+import cat.hack3.mangrana.google.api.client.gateway.GoogleDriveApiGateway;
+import com.google.api.services.drive.model.File;
+
+import java.io.IOException;
+import java.util.List;
+
+public class ArtistCopyService {
+
+    GoogleDriveApiGateway driveApiGateway;
+
+    public ArtistCopyService() throws IOException {
+        this.driveApiGateway = new GoogleDriveApiGateway();
+    }
+
+    public void moveNextArtistFromFolder(String idOriginFolder, String idDestinationFolder) throws IOException {
+        List<File> artists = driveApiGateway.getSomeChildrenFoldersById(idOriginFolder);
+        moveNextArtist(artists, idDestinationFolder, idOriginFolder);
+    }
+
+    private void moveNextArtist(List<File> artists, String originId, String destinationId) throws IOException {
+        File file = artists.get(0);
+        driveApiGateway.moveFolder(file.getId(), originId, destinationId);
+    }
+
+}
---
+++ src/main/java/cat/hack3/mangrana/music/handler/TemporaryFolderAggregator.java
@@ -0,0 +1,28 @@
+package cat.hack3.mangrana.music.handler;
+
+import cat.hack3.mangrana.config.ConfigFileLoader;
+import cat.hack3.mangrana.exception.IncorrectWorkingReferencesException;
+import cat.hack3.mangrana.google.api.client.ArtistCopyService;
+
+import java.io.IOException;
+
+public class TemporaryFolderAggregator {
+
+    ConfigFileLoader configFileLoader;
+    ArtistCopyService artistCopyService;
+
+    public TemporaryFolderAggregator() throws IncorrectWorkingReferencesException {
+        this.configFileLoader = new ConfigFileLoader();
+    }
+
+    public static void main(String[] args) throws IncorrectWorkingReferencesException, IOException {
+        new TemporaryFolderAggregator().moveOneArtist();
+    }
+
+    private void moveOneArtist() throws IOException {
+        artistCopyService.
+                moveNextArtistFromFolder(configFileLoader.getOriginFolderId(),
+                                            configFileLoader.getDestinationFolderId());
+    }
+
+}
