39c70a6fa884a3f15a957e5034f19df6632a653b
Xavier Fontanet <xavier.fontanet@edreamsodigeo.com>
2022-07-30 18:54:15 +0200

    wait one minute before plex refresh

--- src/main/java/cat/hack3/mangrana/music/handler/TemporaryFolderAggregator.java
+++ src/main/java/cat/hack3/mangrana/music/handler/TemporaryFolderAggregator.java
@@ -6,6 +6,7 @@ import cat.hack3.mangrana.google.api.client.ArtistCopyService;
 import cat.hack3.mangrana.plex.url.PlexCommandLauncher;
 
 import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
 import static cat.hack3.mangrana.config.ConfigFileLoader.ProjectConfiguration.DESTINATION_FOLDER_ID;
 import static cat.hack3.mangrana.config.ConfigFileLoader.ProjectConfiguration.ORIGIN_FOLDER_ID;
@@ -17,7 +18,7 @@ public class TemporaryFolderAggregator {
     private final ArtistCopyService artistCopyService;
     private final PlexCommandLauncher plexCommandLauncher;
 
-    public TemporaryFolderAggregator() throws IncorrectWorkingReferencesException, IOException {
+    public TemporaryFolderAggregator() throws IOException, IncorrectWorkingReferencesException {
         this.artistCopyService = new ArtistCopyService();
         this.configFileLoader = new ConfigFileLoader();
         this.plexCommandLauncher = new PlexCommandLauncher(configFileLoader);
@@ -28,16 +29,32 @@ public class TemporaryFolderAggregator {
         new TemporaryFolderAggregator().aggregateNextArtist();
     }
 
-    private void aggregateNextArtist() throws IOException {
-        String artistName = moveOneArtist();
-        log("successfully moved artist: "+artistName);
-        plexCommandLauncher.refreshArtist(artistName);
+    private void aggregateNextArtist() {
+        try {
+            String artistName = moveOneArtist();
+            log("successfully moved artist: "+artistName);
+            waitOneMinute();
+            plexCommandLauncher.refreshArtist(artistName);
+        } catch (Exception e) {
+            log("something went wrong with the artist aggregation");
+            e.printStackTrace();
+        }
     }
 
     private String moveOneArtist() throws IOException {
         return artistCopyService.
                 moveNextArtistFromFolder(configFileLoader.getConfig(ORIGIN_FOLDER_ID),
                                          configFileLoader.getConfig(DESTINATION_FOLDER_ID));
+    }
+
+    private void waitOneMinute() {
+        log("wait 1 minute so the rclone mount realise the change...");
+        try {
+            TimeUnit.MINUTES.sleep(1);
+        } catch (InterruptedException e) {
+            log("waiting went wrong");
+            Thread.currentThread().interrupt();
+        }
     }
 
 }
